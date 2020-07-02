4dae49e3f50d5685f2b437626acfe1171c051e4a
Scott Petersen <scott@digression.org>
2020-07-01 23:35:06 -0700

    Disabled old Pytheos class tests until I rework them.

--- tests/test_pytheos.py
+++ tests/test_pytheos.py
@@ -10,26 +10,26 @@ import pytheos
 
 
 class TestPytheos(unittest.TestCase):
-    def setUp(self) -> None:
-        self._pytheos = pytheos.Pytheos('10.7.2.64', 1255)
-        self._pytheos.connect()
-
-    def tearDown(self) -> None:
-        self._pytheos.close()
-
-    def test_discover(self):
-        discovered = pytheos.discover()
-        self.assertIsInstance(discovered[0], pytheos.Pytheos)
-
-    def test_connect(self):
-        server = '10.7.2.64:1255'
-
-        with pytheos.connect(server) as connection:
-            self.assertIsNotNone(connection)
-
-    def test_system_heartbeat(self):
-        self._pytheos.call('system', 'heart_beat')
-
+    # def setUp(self) -> None:
+    #     self._pytheos = pytheos.Pytheos('10.7.2.64', 1255)
+    #     self._pytheos.connect()
+    #
+    # def tearDown(self) -> None:
+    #     self._pytheos.close()
+    #
+    # def test_discover(self):
+    #     discovered = pytheos.discover()
+    #     self.assertIsInstance(discovered[0], pytheos.Pytheos)
+    #
+    # def test_connect(self):
+    #     server = '10.7.2.64:1255'
+    #
+    #     with pytheos.connect(server) as connection:
+    #         self.assertIsNotNone(connection)
+    #
+    # def test_system_heartbeat(self):
+    #     self._pytheos.call('system', 'heart_beat')
+    pass # FIXME: Rework these.
 
 if __name__== '__main__':
     unittest.main()
