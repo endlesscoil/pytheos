362629a15e175e418c8cafc5a0d1d6a735363e0d
Scott Petersen <scott@digression.org>
2021-02-15 10:43:16 -0800

    Initial async version of the discovery.

--- pytheos/networking/types.py
+++ pytheos/networking/types.py
@@ -19,3 +19,28 @@ class SSDPResponse(http.client.HTTPResponse):
 
     def __repr__(self):
         return f"<SSDPResponse(location={self.location})>"
+
+
+class NewSSDPResponse:
+    def __init__(self, response):
+        self.headers = {}
+
+        fields = response.split('\r\n')
+        if len(fields) == 0:
+            raise ValueError('Invalid SSDP Response')
+
+        self.protocol, self.response_code, self.response = fields[0].split(' ')
+        for f in fields[1:]:
+            split_values = f.split(':')
+
+            field_name = split_values[0]
+            field_value = ':'.join(split_values[1:]).strip()
+            if field_name == '':
+                continue
+
+            self.headers[field_name] = field_value
+
+        self.location = self.headers.get('LOCATION')
+
+    def __repr__(self):
+        return f"<SSDPResponse(location={self.location})>"
